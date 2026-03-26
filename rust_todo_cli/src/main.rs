ac1f7566fc1460f705ec394f9acfaadb16a8cd27
mscode07 <msabhithakur7777@gmail.com>
2026-03-26 12:19:54 +0530

    working on todo application in RUST

--- rust_todo_cli/src/main.rs
+++ rust_todo_cli/src/main.rs
@@ -4,16 +4,25 @@ fn main(){
     let args: Vec<String> = env::args().collect();
     if args.len()<2{
         println!("Usage: add/list");
+        println!("args: {:?}",args.len());
         return;
     }
+    println!("args: {:?}",args);
     let command = &args[1];
+    let mut tasks: Vec<String> = Vec::new();
 
-    if command == "add"{
+    if command =="add"{
+        if args.len() < 3{
+            println!("Please Provide a Task to add");
+            return;
+        }
         let task = &args[2];
-        println!("Adding Task: {}",task);
-    } else if command == "list"{
-        println!("Listing Tasks...");
-    } else {
-        println!("unknown command");
+        tasks.push(task.to_string());
+        println!("Task added: {}", task);
+    } else if command == "list"{  
+        for (i,task) in tasks.iter().enumerate(){
+            println!("{}: {}",i + 1, task);
+        }
+
     }
 }
