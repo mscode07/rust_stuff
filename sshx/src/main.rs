57a94101a051dd0703958150e8b407840b57fdb0
mscode07 <msabhithakur7777@gmail.com>
2026-04-05 20:58:31 +0530

    working on ssh project

--- sshx/src/main.rs
+++ sshx/src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser,Subcommand};
+#[derive(Parser)]
+#[command(name="sshx")]
+#[command (about="Secure SSH Key manager",long_about=None)]
+
+struct Cli{
+    #[command(subcommand)]
+    command: Commands
+}
+#[derive(Subcommand)]
+enum Commands {
+    Generate{
+        #[arg(short,long)]
+        name:String,
+    },
+    List,
+    Get{
+        name:String,
+        #[arg(long)]
+        public: bool
+    }
+}
 fn main() {
     println!("Hello, world!");
     println!("Testing");
