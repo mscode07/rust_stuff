55f96ebc0b8453d960a13197e827b8b7e5fbf23a
mscode07 <msabhithakur7777@gmail.com>
2026-05-06 17:11:36 +0530

    Phase 3 done, starting phase 4

--- sshx/src/crypto/ssh.rs
+++ sshx/src/crypto/ssh.rs
@@ -1,8 +1,8 @@
  
-use ssh_key::{private::PrivateKey, Algorithm, LineEnding};
+use ssh_key::{private::PrivateKey};
 use rand_core::OsRng;
 
-pub fn generate_ed25519_key(name: &str)->(String, String){
+pub fn generate_ed25519_key(_name: &str)->(String, String){
     let mut rng = OsRng;
     let private_key = PrivateKey::random(&mut rng, ssh_key::Algorithm::Ed25519).expect("Faild to generate Key");
 
